cd76127da767c76272a31f7b3b2bfb959980098b
OlenaKovalenko <elenkow17@gmail.com>
2023-10-28 21:07:50 +0300

    Create Prolile.js

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,38 @@
+
+
+// <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+    return (
+        <div className="profile">
+            <div className="description">
+                <img src={avatar} alt={username} width="100"  className="avatar" />
+                <p className="name">{username}</p>
+                <p className="tag">@{tag}</p>
+                <p className="location">{location}</p>
+            </div>
+
+            <ul className="stats">
+                <li>
+                    <span className="label">Followers</span>
+                    <span className="quantity">{stats.followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views</span>
+                    <span className="quantity">{stats.views}</span>
+                </li>
+                <li>
+                    <span className="label">Likes</span>
+                    <span className="quantity">{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
